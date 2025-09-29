778db099c4907feb18ccf0f22cfef7a8b1cf1aac
WOOTAEKJO <tbxjqh1205@naver.com>
2025-09-29 23:56:58 +0900

    Telemetry, Assert 역할 주석

--- Engine/Public/Assert.h
+++ Engine/Public/Assert.h
@@ -1,5 +1,7 @@
 #pragma once
 
+// 역할 : Dev/Soft/Hard 3단계 어서션 + 모드(Dev/Release) 정책 + 실패 콜백
+// 역할 : 실패 시 Logger로 표준 포맷 ERROR|CORE|"... assert failed: ..." 남기고, 모드/정책에 따라 Break(디버그 브레이크) / Continue / Exit 수행.
 
 void DevAssert() {}
 
--- Engine/Public/Telemetry.h
+++ Engine/Public/Telemetry.h
@@ -5,8 +5,9 @@
 	FrameMs, FPS, MemMB, JobsQueued, JobsRunning, JobLatencyMs, SpawnPerSec, ReleasePerSec, ExhaustRatePct,
 	GrowCount, AvgHoldMs, HandleInvalid, IOPending, ApplyQueue
 
-
 */
+
+// 역할 : 카운터/게이지/윈도우 평균/히스토그램(경량) + HUD 3줄 문자열 생성
 
 void Counter() {}
 
