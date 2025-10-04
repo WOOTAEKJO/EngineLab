f30eabec79180f7cc3761b42fadbb4b47d5e015f
WOOTAEKJO <tbxjqh1205@naver.com>
2025-10-04 23:33:25 +0900

    로거 작업

--- Engine/Public/Logger.h
+++ Engine/Public/Logger.h
@@ -1,11 +1,45 @@
 #pragma once
+#include "LogSinkConsole.h"
+#include "LogSinkFile.h"
+#include "LogRingBuffer.h"
 
 // 역할 : 중앙 로거 - 포멧팅, 레이트 리밋(초당 1회 상세), 프레임 스탬핑, 멀티 싱크 브로드 캐스트
 
-void Init() {}
+BEGIN(Engine::log)
 
-void Write() {}
+// 키 값으로 쓰기 위한 데이터 묶음
+struct RateKey
+{
+	bool operator==(const RateKey& o) const
+	{
+		return (m_lev == o.m_lev) && (m_cat == o.m_cat) && (m_msg == o.m_msg);
+	}// 연산자 오버로딩을 통해 키 체크
 
-void FrameStart() {}
+	LEVEL		m_lev;
+	CATEGORY	m_cat;
+	string		m_msg;
+};
 
-void FrameEnd() {}
+// RateKey 구조체 키를 위한 해쉬 함수
+struct RateKeyHash
+{
+	size_t operator()(const RateKey& k) const {
+		return std::hash<int>()((int)k.m_lev * 131 + (int)k.m_cat * 17) ^ std::hash<std::string>()(k.m_msg);
+	}
+};
+
+class Logger
+{
+protected:
+	Logger() = default;
+	~Logger() = default;
+
+public:
+
+private:
+
+private:
+
+};
+
+END
--- Reference/Headers/LogRingBuffer.h
+++ Reference/Headers/LogRingBuffer.h
@@ -1,3 +1,43 @@
 #pragma once
+#include "LogSink.h"
+
+#define MaxMsgNum 2000
 
 // 역할 : 최근 N(기본 2000개)줄 메모리 보관(크래시 직전 로그 확인 용도)
+
+BEGIN(Engine::log)
+
+class RingBufferSink : public ISink
+{
+public:
+	RingBufferSink() = default;
+	RingBufferSink(size_t num = MaxMsgNum)
+		:m_MsgMaxNum(num)
+	{ }
+	virtual ~RingBufferSink() = default;
+
+	virtual void OnMessage(const Event& e, const string& formatted) override
+	{
+		std::lock_guard<std::mutex> lk(m_Mutex); // 뮤텍스 잠금
+
+		if (m_MsgLine.size() == m_MsgMaxNum)
+			m_MsgLine.pop_front(); // 메시지 최대 갯수를 넘으면 제일 앞(오래된)에 메시지를 삭제처리
+
+		m_MsgLine.push_back(formatted);
+	}
+
+	// 스냅샷 시점의 복사본을 돌려주며, 내부 버퍼는 그대로 유지.
+	vector<string> Snapshot()
+	{
+		lock_guard<mutex>	Ik(m_Mutex); // 뮤텍스 잠금
+		return { m_MsgLine.begin(), m_MsgLine.end() }; // 처음부터 끝까지 복사하여 벡터로 반환한다.
+	}
+
+private:
+	size_t			m_MsgMaxNum;	// 보관할 최대 메시지 수
+	deque<string>	m_MsgLine;		// 메시지를 줄 단위로 저장
+									// deque -> 양쪽 끝에서 빠르게 넣고 빼기가 가능한 시퀀스
+	mutex			m_Mutex;		// 한 번에 하나의 스레드만 들어오게 하기 위한 잠금용 변수
+};
+
+END
